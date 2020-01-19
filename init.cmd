90209d4d3f6a0217eafbe68b533594c4d7d159c6
spivegin <spivegin@txtsme.com>
2020-01-18 20:07:24 -0500

    added logos

---
+++ init.cmd
@@ -0,0 +1,6 @@
+echo "# tricll.github.io" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/tricll/tricll.github.io.git
+git push -u origin master
